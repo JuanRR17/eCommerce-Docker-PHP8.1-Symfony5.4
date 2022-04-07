e4a52f44be6890580d71433d727795ce4182642e
JuanRR17 <juanrepeto@gmail.com>
2022-04-07 20:33:09 +0200

    salt made nullable in User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+// use FOS\UserBundle\Model\User as BaseUser;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -107,7 +108,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'datetime_immutable')]
     private $created_at;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $salt;
 
     #[ORM\OneToMany(mappedBy: 'userid', targetEntity: Basket::class)]
@@ -120,6 +121,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __construct()
     {
+        // parent::__construct();
         $this->orders = new ArrayCollection();
         $this->baskets = new ArrayCollection();
     }
